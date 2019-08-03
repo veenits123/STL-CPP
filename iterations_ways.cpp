b84a91832187f56c8ab91e04335a7694b874a431
VEENIT SINGH <123sveenit@gmail.com>
2019-08-03 10:47:21 +0530

    cnf-1C

---
+++ iterations_ways.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int32_t main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	//  code starts
+	vector<int> v;
+	v.pb(3);
+	v.pb(2);
+	for(int i=0;i<v.size();i++){
+		cout<<v[i]<<'\n';
+	}
+	vector<int>::iterator it;
+	for(it=v.begin();it!=v.end();it++){
+		cout<<*it<<'\n';
+	}
+	for(int x:v){
+		cout<<x<<'\n';
+	}
+	for(auto x:v){
+		cout<<x<<'\n';
+	}
+}
