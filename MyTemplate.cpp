fca3ea8259149018ba20d8995a7b1f83016f1a62
VEENIT SINGH <123sveenit@gmail.com>
2019-09-12 09:22:51 +0530

    shvsx

---
+++ MyTemplate.cpp
@@ -0,0 +1,49 @@
+
+/*              `-._:  .:'   `:::  .:\            |\__/|           /::  .:'   `:::  .:.-'
+                    \      : V         \          |:   |          /         : T      /    
+                     \     ::    .     `-_______/ ::   \_______-'   .      ::   . /      
+                      |  :   :: ::'  :   :: ::'  :   :: ::'    N  :: ::'  :   :: :|       
+                      |     ;::        E ;::         ;::         ;::         ;::  |       
+                      |  .:'   `:::  .:'   `:::  .:'   `:::  .:'   `:::  .:'   `:|       
+                      /     :           :         E  :           :     I      :    \       
+                     /______::_____     ::    .     ::    .     ::   _____._::____\      
+                                   `----._:: ::'  :   :: ::'  _.----'                    
+                                          `--.       ;::  .--'                           
+                                              `-. .:'  .-'                               
+                                                 \    /                        
+                                                  \  /                                   
+                                                   \/  */             
+                
+// Author : vee_nits123
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define ld long double
+#define F first
+#define S second
+#define pb push_back
+#define pbk pop_back
+#define PI 3.1415926535897932384626
+#define endl '\n'
+const int mod = 1000000007;
+const int N=100005;
+
+void the_Bloombug(){
+    ios_base:: sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("debug.txt", "w", stderr);
+    #endif
+}
+
+int32_t main(){
+  
+  the_Bloombug();
+  
+  //int t;cin>>t;while(t--){}
+
+  return 0;
+}
